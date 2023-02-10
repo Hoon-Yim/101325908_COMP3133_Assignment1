0784da7727de415fce87abe6157e051a536606ad
Hoon Yim <yimsh4507@gmail.com>
2023-02-09 19:52:07 -0500

    Updated userModel.js - added password encryption and email format validation features

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
 const userSchema = mongoose.Schema({
     username: {
         type: String,
@@ -9,16 +12,26 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         unique: true,
+        validate: [validator.isEmail, "Given email is in wrong format"],
         required: [true, "An email is required"]
     },
     password: {
         type: String,
+        minlength: 8,
         required: [true, "A password is required"]
     }
 });
 
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    // Encrypting password
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
 userSchema.methods.isPasswordCorrect = async function(givenPassword, userPassword) {
-    return givenPassword === userPassword;
+    return await bcrypt.compare(givenPassword, userPassword);
 }
 
 module.exports = mongoose.model("User", userSchema);
